864a9168bc5e6d8293948af9c247bf6e124b1c16
CJConnor <38636000+CJConnor@users.noreply.github.com>
2019-03-28 11:06:10 +0000

    Update user.php

--- classes/user.php
+++ classes/user.php
@@ -2,7 +2,38 @@
 
   class User extends Sytem {
   
+    public $id;
+    public $username;
+    public $password;
+    public $forename;
+    public $surname;
+    public $dob;
+    public $age
+    public $fav_team;
+    public $admin;
+   
+    public static $tableName = "users";
+    public static $columns   = ['id', 'username', 'password', 'forename', 'surname', 'dob', 'age', 'fav_team', 'admin'];
     
+    public function __construct($args=[]) {
+      
+        $this->id       = isset($args['id']) ? $args['id'] : "";
+        $this->username = isset($args['username']) ? $args['username'] : "";
+        $this->password = isset($args['password']) ? $args['password'] : "";
+        $this->forename = isset($args['forename']) ? $args['forename'] : "";
+        $this->surname  = isset($args['surname']) ? $args['surname'] : "";
+        $this->dob      = isset($args['dob']) ? $args['dob'] : "";
+        $this->age      = isset($args['age']) ? $args['age'] : "";
+        $this->fav_team = isset($args['fav_team']) ? $args['fav_team'] : "";
+        $this->admin    = isset($args['admin']) ? $args['admin'] : "";
+    
+      
+    }
+		
+		public function createUser() {
+				//Function to register user	
+			
+		}
   
   }
 
